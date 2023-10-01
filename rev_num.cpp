03169e2a37563a5eec32fe065a26fda7d9cf4edb
tiqwell <98182406+tiqwell@users.noreply.github.com>
2023-10-01 16:05:24 +0300

    Update rev_num.cpp

--- rev_num.cpp
+++ rev_num.cpp
@@ -3,10 +3,11 @@ int itc_rev_num(long long number) {
 	long long a = 0;
 	if (number < 0) 
 		number *= -1; 
-	while (number = number /10) 
+	while (number) 
 	{
 		int i = number % 10;
 		a = a * 10 + (i * 10);
+		number /= 10;
 	}
 	a /= 10;
 	return itc_len_num(a);
